412fd49113166c2b8243e7573e38d5a633feedf4
Akshay <akshaycm@hotmail.com>
2019-12-10 00:41:35 +0300

    add onjoin validation of blacklist

--- index.js
+++ index.js
@@ -10,16 +10,31 @@ const blacklistedavatars = config.blacklistedavatars;
 //whitelist the real giveaway bot and nogiveaway bot
 const whitelistedids = config.whitelistedids;
 var extrablacklist = config.blacklistedidsextra;
+var blacklistedids = [];
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity(`Protecting ${client.guilds.size} servers from giveaway spam`);
 });
+
 client.on("guildCreate", guild => {
     // This event triggers when the bot joins a guild.
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
     client.user.setActivity(`Protecting ${client.guilds.size} servers from giveaway spam`);
 });
-var blacklistedids = [];
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    console.log(member)
+    checkForBlacklistedAvatarandBan(member)
+    // Send the message, mentioning the member
+    channel.send(`Welcome to the server, ${member}`);
+  });
+
 client.on('message', msg => {
     //Check if there is a guild in message,dont go further if its a dm.
     if (!msg.guild) return;
@@ -32,17 +47,18 @@ client.on('message', msg => {
     } else if (msg.content === 'getblacklistcount') {
         msg.reply(blacklistedmatches);
     } else if (msg.content === 'banBlacklisted') {
-        banBlacklisted(msg);
+        banBlacklisted(msg,false,null);
     } else if (msg.content === 'banextra') {
-        banBlacklisted(msg, true);
+        banBlacklisted(msg, true,null);
     }
 });
 client.login(config.token);
 async function buildBlacklist(msg) {
-    if (blacklistedids.length > 0 || bancount > 0) {
+    if (blacklistedids.length > 0 || bancount > 0 || blacklistedmatches > 0) {
         //reset fields,just incase as we only support one server for now per instance
         blacklistedids = [];
         bancount = 0
+        blacklistedmatches = 0
     }
     list = msg.guild;
     if (list != undefined)
@@ -73,7 +89,16 @@ function checkForBlacklistedAvatar(user) {
         }
     });
 }
-async function banBlacklisted(msg, fBanExtra) {
+function checkForBlacklistedAvatarandBan(user) {
+    blacklistedavatars.forEach(function(item) {
+        if (user.user.avatar != null && user.user.avatar.includes(item.toString())) {
+            console.log("Adding blacklisted userid :" + user.id);
+            blacklistedmatches = blacklistedids.push(user.user.id);
+        }
+    });
+    banBlacklisted(null,false,user)
+}
+async function banBlacklisted(msg, fBanExtra,memberx) {
     if (bancount == blacklistedmatches) {
         const banConfirmationEmbedModlog = new Discord.RichEmbed()
             .setAuthor(`Banned Spammers by **${msg.author.username}#${msg.author.discriminator}**`, msg.author.displayAvatarURL)
@@ -85,10 +110,21 @@ async function banBlacklisted(msg, fBanExtra) {
         client.channels.get(msg.channel.id).send({
             embed: banConfirmationEmbedModlog
         }); // Sends the RichEmbed in the modlogchannel
+        //Clear bancount and blacklist after clearing
+                //reset fields,just incase as we only support one server for now per instance
+                blacklistedids = [];
+                bancount = 0;
+                blacklistedmatches = 0;
     } else {
         if (fBanExtra) {blacklistedids.concat(extrablacklist)}
         blacklistedids.forEach(function(item) {
-            const member = msg.guild.member(item);
+
+            var member;
+            if(msg != null)
+               member = msg.guild.member(item);
+            else
+                member = memberx
+
             if (member) {
                 /**
                  * Ban the member
