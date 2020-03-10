ddc41d444b564d8d48652a9c55ea70226c35d99a
akshaynexus <akshaycm@hotmail.com>
2020-03-10 19:58:20 +0000

    Remove debug code from blacklistedid export

--- testdb.js
+++ testdb.js
@@ -4,20 +4,9 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 var configids = config.blacklistedids; 
 var blacklistedids = []
-mongoose.connect('mongodb://' + config.db.user + ':' + config.db.pass + '@' +'localhost/nogiveaway', function (err) {
 
+mongoose.connect('mongodb://' + config.db.user + ':' + config.db.pass + '@' +'localhost/nogiveaway', function (err) {
     if (err) throw err;
-    // //Save config and blacklist stuff to db
-    // const configtosave = new Config({
-    //     _id: new mongoose.Types.ObjectId(),
-    //     blacklistedavatars : configjson.blacklistedavatars,
-    //     blacklistedids: configjson.blacklistedids,
-    //     token:configjson.token,
-    //     blacklistednames : configjson.blacklistednames,
-    //     whitelistedids : configjson.whitelistedids,
-    // })
-    // configtosave.save();
-    //Login to discord with token
     BlacklistIDModel.find({}, function(err, data){
         if(err){
           console.log(err);
@@ -25,9 +14,7 @@ mongoose.connect('mongodb://' + config.db.user + ':' + config.db.pass + '@' +'lo
         if(data.length == 0) {
             console.log("nothing gotten")
         }
-        // console.log(data.length)
         for(var i=0;i<data.length;i++){
-            // console.log(data[i].userid.length)
             if(data[i].userid.length == 1){
                 blacklistedids.push(parseFloat(data[i].userid.toString()));
             }
@@ -40,9 +27,7 @@ mongoose.connect('mongodb://' + config.db.user + ':' + config.db.pass + '@' +'lo
       });
 }, {useNewUrlParser: true});
 
-// console.log(ServerJoin.find({"userid": 685920414580736049}))
 function saveblacklist(){
-    // console.log(blacklistedids)
     blacklistedids.concat(configids)
     blacklistedids = [...new Set(blacklistedids)];
     fs.writeFile("outputx.json", JSON.stringify({blacklistedids:blacklistedids},null, 4), 'utf8', function (err) {
